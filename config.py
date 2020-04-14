e3624a93f0400844c4ee049d582a9a53417b81a0
SayYoungMan <nickte89@gmail.com>
2020-04-14 18:45:12 +0100

    Cumulative data added, config file added

---
+++ config.py
@@ -0,0 +1,14 @@
+countries_codes = [
+    "AFG", "ALB", "DZA", "AND", "AGO", "ATG", "ARG", "ARM", "ABW", "AUS", "AUT", "AZE", "BHS", "BHR", "BGD", "BRB", "BLR",
+    "BEL", "BLZ", "BEN", "BMU", "BTN", "BOL", "BIH", "BWA", "BRA", "VGB", "BRN", "BGR", "BFA", "BDI", "KHM", "CMR", "CAN",
+    "CPV", "CYM", "CAF", "TCD", "CHL", "CHN", "COL", "COG", "CRI", "CIV", "HRV", "CUB", "CUW", "CYP", "COD", "DNK", "DJI",
+    "DMA", "DOM", "ECU", "EGY", "SLV", "GNQ", "ERI", "EST", "SWZ", "ETH", "FRO", "FJI", "FIN", "FRA", "PYF", "GAB", "GEO",
+    "DEU", "GHA", "GIB", "GRC", "GRL", "GRD", "GUM", "GTM", "GGY", "GIN", "GNB", "GUY", "HTI", "HND", "HUN", "ISL", "IND",
+    "IDN", "IRN", "IRQ", "IRL", "IMN", "ISR", "ITA", "JAM", "JPN", "JEY", "JOR", "KAZ", "KEN", "XKX", "KWT", "KGZ", "LAO",
+    "LVA", "LBN", "LBR", "LBY", "LIE", "LTU", "LUX", "MDG", "MWI", "MYS", "MDV", "MLI", "MLT", "MRT", "MUS", "MEX", "MDA",
+    "MCO", "MNG", "MNE", "MSR", "MAR", "MOZ", "MMR", "NAM", "NPL", "NLD", "NCL", "NZL", "NIC", "NER", "NGA", "MKD", "MNP",
+    "NOR", "OMN", "PAK", "PSE", "PAN", "PNG", "PRY", "PER", "PHL", "POL", "PRT", "PRI", "QAT", "ROU", "RUS", "RWA", "KNA",
+    "LCA", "VCT", "SMR", "STP", "SAU", "SEN", "SRB", "SYC", "SLE", "SGP", "SXM", "SVK", "SVN", "SOM", "ZAF", "KOR", "SSD",
+    "ESP", "LKA", "SDN", "SUR", "SWE", "CHE", "SYR", "TWN", "THA", "TLS", "TGO", "TTO", "TUN", "TUR", "TCA", "UGA", "UKR",
+    "ARE", "GBR", "TZA", "USA", "VIR", "URY", "UZB", "VEN", "VNM", "YEM", "ZMB", "ZWE"
+]
--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-'''
-TO DO: import database and divide by countries
-'''
 
 import os
 import pandas as pd
+import matplotlib.pyplot as plt
 
 DATA_PATH = os.path.join(os.getcwd(), "Data\COVID-19-geographic-disbtribution-worldwide.xlsx")
 
@@ -12,7 +10,7 @@ def data_preprocess(df):
     :param df:
     :return drop all the columns from the dataframe except the needed ones:
     """
-    return df.filter(["dateRep", "cases", "deaths", "countriesAndTerritories", "popData2018"])
+    return df.filter(["dateRep", "cases", "deaths", "countryterritoryCode", "popData2018"])
 
 def get_country(country_code):
     """
@@ -21,6 +19,35 @@ def get_country(country_code):
     """
     return df.loc[df["countryterritoryCode"] == country_code]
 
-df = pd.read_excel(DATA_PATH)
-df = data_preprocess(df)
-print(df.head())
+def cumulative_data(ds):
+    """
+    :param ds:
+    :return the new dataset with two columns added:
+        1. Cumulative cases till the day
+        2. Cumulative deaths till the day:
+    """
+    ds["cum_cases"] = ds["cases"].cumsum()
+    ds["cum_deaths"] = ds["deaths"].cumsum()
+    return ds
+
+def time_process(ds):
+    """
+    converts dateRep column into datetime object and sort it according to the date
+    :param ds:
+    :return new dataset:
+    """
+    ds["dateRep"] = pd.to_datetime(ds.dateRep)
+    ds = ds.sort_values(by="dateRep")
+    return ds
+
+def process_by_country(country_code):
+    country_ds = get_country(country_code)
+    country_ds = time_process(country_ds)
+    country_ds = cumulative_data(country_ds)
+    return country_ds
+
+
+df = data_preprocess(pd.read_excel(DATA_PATH))
+
+kor = process_by_country("KOR")
+print(kor)
